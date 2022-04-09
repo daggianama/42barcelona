57f087756382dd4a5cc97936fe432f52ee70f39e
daggianama <70452865+daggianama@users.noreply.github.com>
2022-04-09 18:13:27 +0200

    Add files via upload

---
+++ rush01/rush01/cajas.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cajas.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmadrid- <dmadrid-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/09 17:22:59 by dmadrid-          #+#    #+#             */
+/*   Updated: 2022/04/09 18:13:12 by dmadrid-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+void rev_string()
+{
+	int i; // i es el nuemro de caracteres en el string
+	i = 0; 
+		
+	if(srt[i] != 0) 
+		return (0);
+
+	while (i <= 31)
+	{
+		if (i/2 == 0)    // si el string contiene nada o es null
+		return (0); 
+		else if (str[i] < 0 && > 4)  // determina si los valores estan como deben en posicion par
+		return (0);	
+		else
+		i++;
+	}
+
+	if (i == 31) 
+	{ 
+		str[i] 
+
+
+	
+
+
+
+
+
+		
+
+
+ 
+
+	}
+}
+
