80637cecaaecac93fc78d171dc7fdd33eeee2e16
daggianama <70452865+daggianama@users.noreply.github.com>
2022-04-09 23:45:30 +0200

    Add files via upload

---
+++ dagrush01/argv_pre.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	int i; // indica el movimiento de lectura en el string, usado para el while loop 
+	i = 0; 
+	
+	while (i<=argc)
+	{
+		printf("argumento %s\n", argv[i]);
+		i++;
+	}
+
+
+	
+	if(argc != 2) 
+		return (0);
+	else
+	{
+		imput = check_string (argv[1]);
+		if (imput == 0)
+		       return (0);
+	}
+	if (argv[i] > 4 || argv[i] < 0)	
+		return (0);
+
+	else
+	{
+		arr = (int*) malloc (4*4 sizeof(int))
+	}
+
+
+
+
+
+
+
+
+	while (i <= 31)
+	{
+		if (i/2 == 0)    // si el string contiene nada o es null
+		return (0); 
+		else if (str[i] < 0 && > 4)  // determina si los valores estan como deben en posicion par
+		return (0);	
+		else
+		i++;
+	}
+
+	if (i == 31) 
+	{ 
+		str[i] 
+*/
