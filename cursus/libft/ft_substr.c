11d3aa165f0f0eda1026425acd73fe4bfc7f2602
daggianama <70452865+daggianama@users.noreply.github.com>
2022-05-21 13:40:23 +0200

    Create ft_substr.c

---
+++ cursus/libft/ft_substr.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+
+** LIBRARY: N/A
+** SYNOPSIS: extract substring from string
+**
+** DESCRIPTION:
+** 		Allocates (with malloc(3)) and returns a substring from the string ’s’.
+**	The substring begins at index ’start’ and is of maximum size ’len’.
+*/
+  
+  
