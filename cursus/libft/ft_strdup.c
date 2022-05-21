3a9d992cce2dc5fdad8e737df1d1f0f237157951
daggianama <70452865+daggianama@users.noreply.github.com>
2022-05-21 13:03:04 +0200

    Update ft_strdup.c
    comentarios de info agregados

--- cursus/libft/ft_strdup.c
+++ cursus/libft/ft_strdup.c
@@ -1,3 +1,15 @@
+#include "libft.h"
+/*
+** SYNOPSIS: save a copy of a string (with malloc, size-bounded)
+**
+** DESCRIPTION:
+** 		The strdup() function allocates sufficient memory for a copy of the
+**	string s1, does the copy, and returns a pointer to it.  The pointer may
+**	subsequently be used as an argument to the function free(3).
+** 		The strndup() function is similar, but only copies at most n bytes.  If
+**	s is longer than n, only n bytes are copied, and a terminating null byte
+**	('\0') is added.
+*/
 char *ft_strdup (const char *s)
 {
     char *pt = malloc (strlen (s) + 1);   // allocate memory
