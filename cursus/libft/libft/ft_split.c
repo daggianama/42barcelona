1ab5fc2639e84daf7e48f2c7b02c8647cac12876
daggianama <70452865+daggianama@users.noreply.github.com>
2022-05-24 13:48:17 +0200

    Update ft_split.c

--- cursus/libft/libft/ft_split.c
+++ cursus/libft/libft/ft_split.c
@@ -1,8 +1,17 @@
 #include "libft.h"
 #include <stdlib.h>
 
+int 
+
+
 char **ft_split(char const *s, char c)
 {
+  char **str;
+  
+  str = (char **)malloc(sizeof() * );
+  if (!str)
+    return (NULL);
+  
 }
 
 /*Reserva con malloc(3) y devuelve un array de
