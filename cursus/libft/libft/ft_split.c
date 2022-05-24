ce2899b1e68657a1c20b2c16ac2fd6c5b0e6ee4e
daggianama <70452865+daggianama@users.noreply.github.com>
2022-05-24 12:28:08 +0200

    Create ft_split.c

---
+++ cursus/libft/libft/ft_split.c
@@ -0,0 +1,11 @@
+#include "libft.h"
+#include <stdlib.h>
+
+char **ft_split(char const *s, char c)
+{
+}
+
+/*Reserva con malloc(3) y devuelve un array de
+strings obtenido al separar ’s’ con el caracter ’c’
+como delimitador. El array debe terminar en NULL.
+*/
