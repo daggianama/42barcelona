b047d22d27650004e17e99acf4845baf6432f746
daggianama <70452865+daggianama@users.noreply.github.com>
2022-07-07 18:39:11 +0200

    Add files via upload

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dmadrid- <dmadrid-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 16:12:36 by dmadrid-          #+#    #+#             */
-/*   Updated: 2022/06/21 17:50:26 by dmadrid-         ###   ########.fr       */
+/*   Updated: 2022/07/07 18:04:34 by dmadrid-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libftprintf.h"
@@ -26,10 +26,11 @@ int ft_printf(char const *a, ...)
 	while (a[i])
 	{
 		if (a[i] =="%")
-			if (a[i + 1] == "%")
-			count += ft_putchar(a[i]);
+		{	if (a[i + 1] == "%")
+				count += ft_putchar(a[i]);
 			else
-			count += ft_typeholder(arg, a);
+				count += ft_typeholder(arg, a);
+		}
 		else
 			count += ft_putchar(a[i]);
 	}
@@ -41,6 +42,20 @@ int ft_typeholder(char c, va_list arg)
 {
 	int count;
 
-	while (c == i)
-	count = (va_arg(arg, int));
-	while (c == s)
+	if (c == c)
+		return (ft_putchar(va_arg(arg, char)));
+	if (c == s)
+		return (ft_putstr(va_arg(arg, char *)));
+	if (c == p)
+		return = (va_arg(arg, void));
+	if (c == d || c == i)
+		return (ft_putnbr(va_arg(arg, int));
+	if (c == u)
+		return ((va_arg(arg, unsigned int));
+	if (c == x)
+		return (va_arg(arg, unsigned int));
+	if (c == X)
+		return (va_arg(arg, unsigned int));
+}
+
+		
---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmadrid- <dmadrid-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 17:55:58 by dmadrid-          #+#    #+#             */
+/*   Updated: 2022/06/21 18:32:20 by dmadrid-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef  LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c);
