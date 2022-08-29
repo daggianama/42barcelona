41a3d4db17d0fe92622d5838d2d173b03bca90f8
daggianama <70452865+daggianama@users.noreply.github.com>
2022-08-29 19:00:02 +0200

    Add files via upload

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,26 +6,23 @@
 /*   By: dmadrid- <dmadrid-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 14:52:27 by dmadrid-          #+#    #+#             */
-/*   Updated: 2022/08/08 18:49:49 by dmadrid-         ###   ########.fr       */
+/*   Updated: 2022/08/18 19:49:21 by dmadrid-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <sys/type.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include "get_next_line.h"
 
 char readbuffer(int fd, char *storage)  //utiliza read y reserva mem malloc
 {
-
+	
 }
 
 char get_next_line(int fd) 
 {
-	static char	*saved;
+	static char	*storage;
 	char		*line;
 
-	if (fd < 0)
+	if (fd < 0) //fd = filedescriptor
 		return (NULL);
-	if (!ft_strchr(saved, '\n')
-			storage = readbuf(fd, storage);
+	if (storage && !ft_strchr(storage, '\n') || !storage)
+		storage = readbuf(fd, storage);
 }
